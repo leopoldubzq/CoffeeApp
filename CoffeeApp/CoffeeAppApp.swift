dae0ada6390584c128d775eb663a51f305a54e13
Leopold Romanowski <leopold.romanowski@escolasoft.com>
2023-12-18 19:34:52 +0100

    Initial Commit

---
+++ CoffeeApp/CoffeeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeAppApp.swift
+//  CoffeeApp
+//
+//  Created by Leopold Romanowski on 18/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
