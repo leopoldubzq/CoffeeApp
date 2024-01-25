304ecceaaa0d09387e7a3dac976f8f583d8f1bfd
Leopold Romanowski <leopold.romanowski@escolasoft.com>
2024-01-25 15:37:28 +0100

    Add firebase package to Xcode project, configure Firebase in CoffeeAppApp.swift file

--- CoffeeApp/CoffeeAppApp.swift
+++ CoffeeApp/CoffeeAppApp.swift
@@ -1,9 +1,19 @@
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
 struct CoffeeAppApp: App {
     
     @State private var splashVisible: Bool = true
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
     var body: some Scene {
         WindowGroup {
